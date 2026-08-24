cc94b234b36e5c7cda5b97646d4eee9787ef563d
ksmin930-ai <ksmin930@gmail.com>
2026-08-24 21:24:49 +0900

    Add files via upload

---
+++ api/update.js
@@ -0,0 +1,18 @@
+const { handler } = require('../netlify/functions/update');
+
+module.exports = async (req, res) => {
+  const event = {
+    httpMethod: req.method,
+    headers: req.headers,
+    body: typeof req.body === 'string' ? req.body : JSON.stringify(req.body || {}),
+    queryStringParameters: req.query
+  };
+  const result = await handler(event, {});
+  res.status(result.statusCode);
+  if (result.headers) {
+    for (const [k, v] of Object.entries(result.headers)) {
+      res.setHeader(k, v);
+    }
+  }
+  return res.send(result.body);
+};
